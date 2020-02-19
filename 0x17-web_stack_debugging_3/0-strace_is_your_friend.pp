217fabaf5c420f999e8ca1ef0d3307d3b79d8997
Luis Eduardo D <luis.diaz.car@gmail.com>
2020-02-19 12:57:59 -0500

    fixed path sed and service

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,7 +1,7 @@
 # Fix misconfiguration in Wordpress
 exec { 'replace phpp':
-    command => 'sed -i "s/.phpp/.php/g" /var/www/html/wp-settings.php'
+    command => '/bin/sed -i "s/.phpp/.php/g" /var/www/html/wp-settings.php'
 }
 exec {'restart apache':
-  command => 'service apache2 restart'
+  command => '/usr/sbin/service apache2 restart'
 }
