be7201e0eca1a826da2572d08202af7edcf2d61d
Luis Eduardo D <luis.diaz.car@gmail.com>
2020-02-19 12:19:31 -0500

    add puppet configuration

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,7 @@
+/* Fix misconfiguration in Wordpress */
+exec { 'replace phpp':
+    command => 'sed -i "s/.phpp/.php/g" /var/www/html/wp-settings.php'
+}
+exec {'restart apache':
+  command => 'service apache2 restart'
+}
